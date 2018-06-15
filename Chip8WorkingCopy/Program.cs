0c2ddaa75cc646cfaa9c71ed115f41052550ab39
Gary Moore <madmangaz@icloud.com>
2018-06-15 01:51:48 +0100

    Added nugets for windowing and implemented basic window in main()

---
+++ Chip8WorkingCopy/Program.cs
@@ -0,0 +1,33 @@
+﻿using Veldrid;
+using Veldrid.StartupUtilities;
+
+namespace Chip8WorkingCopy
+{
+    class Program
+    {
+        private static GraphicsDevice _graphicsDevice;
+        
+        static void Main(string[] args)
+        {
+            // Setup windowing system for .NET Core to
+            // display graphics from emulator
+            
+            var windowCi = new WindowCreateInfo
+            {
+                X = 100,
+                Y = 100,
+                WindowWidth = 960,
+                WindowHeight = 540,
+                WindowTitle = "Chip8 Emulator"
+            };
+            var window = VeldridStartup.CreateWindow(ref windowCi);
+
+            _graphicsDevice = VeldridStartup.CreateGraphicsDevice(window);
+
+            while (window.Exists)
+            {
+                window.PumpEvents();
+            }
+        }
+    }
+}
