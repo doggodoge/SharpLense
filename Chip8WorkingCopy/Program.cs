13e6164e9f4524d2ee9525516d8fa20f58865c16
MadManGaz <madmangaz@icloud.com>
2018-07-22 23:49:01 +0100

    Finished 8080 disassembler
    Signed-off-by: Gary Moore <madmangaz@icloud.com>

--- Chip8WorkingCopy/Program.cs
+++ Chip8WorkingCopy/Program.cs
@@ -7,7 +7,7 @@ namespace Chip8WorkingCopy
     {
         static void Main()
         {
-            throw new NotImplementedException();
+            // TODO Convert project to library and rename to 8080Disassembler
         }
     }
 }
